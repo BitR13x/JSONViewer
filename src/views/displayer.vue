ed568f92fd3e58ed289cb52cccf2f9802f00930e
BitR13x <bitr13x@protonmail.com>
2023-01-26 15:27:43 +0100

    css, display JSON in array

--- src/views/displayer.vue
+++ src/views/displayer.vue
@@ -2,8 +2,12 @@
 export default {
     data() {
         return {
+            open: 0 as number,
+            openKeys: [] as string[],
+            openIndex: [] as string[],
             file: null as File | null,
-            data: [{ name: "", href: "", open: false }]
+            data: [] as [{ openid: number }] | {},
+            typeofjson: 1
         }
     },
     methods: {
@@ -19,20 +23,52 @@ export default {
             reader.onload = () => {
                 let text = reader.result;
                 if (typeof (text) !== null) {
-                    this.data = JSON.parse(text as string)
+                    let parsedJSON = JSON.parse(text as string)
+                    if (this.typeofjson === 1) {
+                        let id = 0;
+                        this.data = parsedJSON.map((item: any) => {
+                            return Object.assign({}, item, { openid: id++ })
+                        })
+                    } else if (this.typeofjson === 2) {
+                        this.data = parsedJSON
+                    }
                 }
 
             }
             reader.readAsText(this.file);
         },
-        toggleOpen(id: Number) {
-            this.data.forEach((item, index) => {
-                if (index === id) {
-                    item.open = !item.open;
+        toggleOpen(id: any, type = ""): void {
+            if (this.typeofjson === 2) {
+                if (this.open == id) {
+                    this.open = -1
                 } else {
-                    item.open = false;
+                    this.open = id
                 }
-            });
+            } else if (this.typeofjson === 1) {
+                console.log(id)
+                const loopArray = (Array: string[], key: string) => {
+                    let found = false
+                    Array.forEach((item, index) => {
+                        if (item == key) {
+                            delete Array[index]
+                            found = true
+                            return
+                        }
+                    });
+                    if (!found) Array.push(key)
+                }
+
+                let key = type + String(id)
+                if (type === "index") {
+                    loopArray(this.openIndex, key);
+                }
+                else if (type === "keys") {
+                    loopArray(this.openKeys, key);
+                }
+            }
+        },
+        setTypeOfJSON(type: number): void {
+            this.typeofjson = type
         }
     }
 };
@@ -44,15 +80,40 @@ export default {
             <input type="file" ref="fileInput" @change="handleFileChange" />
             <button @click="handleUpload">Upload</button>
         </div>
+        <div class="TypeButtons">
+            <button :class="{ active: typeofjson === 1 }" @click="setTypeOfJSON(1)">display as JSON in array</button>
+            <button :class="{ active: typeofjson === 2 }" @click="setTypeOfJSON(2)">display as JSON</button>
+        </div>
         <div class="list-names">
-            <ul>
+            <ul v-if="typeofjson === 1">
                 <li v-for="(item, index) in data" :key="index">
-                    <div class="Name" @click="toggleOpen(index)">
-                        {{ item.name }}
+                    <div class="Name" @click="toggleOpen(index, 'index')" :key="index">
+                        {{ index }}
                     </div>
-                    <transition class="Content" name="slide">
-                        <div v-if="item.open" class="content">
-                            <a v-bind:href="item.href" target="_blank">{{ item.href }}</a>
+                    <div class="SecName" v-for="(value, key, index2) in item" :key="index2">
+                        <div v-if="openIndex.includes('index' + String(index))"
+                            @click="toggleOpen(String(item.openid) + String(index2), 'keys')">
+                            {{ key }}
+                        </div>
+                        <transition name="slide">
+                            <div v-if="openKeys.includes('keys' + String(item.openid) + String(index2))"
+                                class="content">
+                                {{ value }}
+                            </div>
+                        </transition>
+                    </div>
+
+
+                </li>
+            </ul>
+            <ul v-else-if="typeofjson === 2">
+                <li v-for="(value, key, index) in data" :key="index">
+                    <div class="Name" @click="toggleOpen(index)">
+                        {{ key }}
+                    </div>
+                    <transition name="slide">
+                        <div v-if="open === index" class="content">
+                            {{ value }}
                         </div>
                     </transition>
                 </li>
@@ -70,6 +131,20 @@ export default {
     padding-left: 5vw;
 }
 
+.SecName {
+    font-size: 2vw;
+    margin-left: 4vw;
+}
+
+.SecName:hover {
+    background-color: rgba(84, 84, 84, 0.65);
+    transition: 0.4s;
+}
+
+.content {
+    margin-left: 4vw;
+}
+
 .Name {
     font-size: 2vw;
 }
@@ -77,6 +152,36 @@ export default {
 .Name:hover {
     background-color: rgba(84, 84, 84, 0.65);
     transition: 0.4s;
+}
+
+.active {
+    background-color: rgb(0, 55, 37);
+}
+
+.TypeButtons {
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    padding-left: 5vw;
+}
+
+button,
+input {
+    background-color: rgb(3, 119, 80);
+    border-radius: 1rem;
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    /* Green */
+    border: none;
+    color: white;
+    margin-right: 1rem;
+    padding: 7.5px 16px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+}
+
+button:hover {
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
+    background-color: rgb(0, 55, 37);
 }
 
 .list-names {
