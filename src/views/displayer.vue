ab625b93875b17cf7645f35122a6bb16d470ffc0
BitR13x <bitr13x@protonmail.com>
2023-01-26 15:32:47 +0100

    css

--- src/views/displayer.vue
+++ src/views/displayer.vue
@@ -45,7 +45,7 @@ export default {
             <button @click="handleUpload">Upload</button>
         </div>
         <div class="list-names">
-            <ul>
+            <ul v-if="data[0].name">
                 <li v-for="(item, index) in data" :key="index">
                     <div class="Name" @click="toggleOpen(index)">
                         {{ item.name }}
@@ -70,6 +70,10 @@ export default {
     padding-left: 5vw;
 }
 
+.content {
+    margin-left: 4vw;
+}
+
 .Name {
     font-size: 2vw;
 }
@@ -77,6 +81,36 @@ export default {
 .Name:hover {
     background-color: rgba(84, 84, 84, 0.65);
     transition: 0.4s;
+}
+
+.active {
+    background-color: rgb(0, 55, 37);
+}
+
+.TypeButtons {
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    padding-left: 5vw;
+}
+
+button,
+input {
+    background-color: rgb(3, 119, 80);
+    border-radius: 1rem;
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    /* Green */
+    border: none;
+    color: white;
+    margin-right: 1rem;
+    padding: 7.5px 16px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+}
+
+button:hover {
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
+    background-color: rgb(0, 55, 37);
 }
 
 .list-names {
